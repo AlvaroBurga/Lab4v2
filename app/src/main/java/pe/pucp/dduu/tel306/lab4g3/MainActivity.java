1b6ed64a1cf18bac03a302a75c0788f4067b5814
Marcelo Garcia <m.garciav@pucp.edu.pe>
2020-11-24 03:16:05 -0500

    marce1

--- app/src/main/java/pe/pucp/dduu/tel306/lab4g3/MainActivity.java
+++ app/src/main/java/pe/pucp/dduu/tel306/lab4g3/MainActivity.java
@@ -2,7 +2,29 @@ package pe.pucp.dduu.tel306.lab4g3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.NetworkInfo;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +34,91 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //hola
         //Adios
+    }
+    public void ahorasineta (View view){
+
+    }
+    public void validarUsuario(View view){
+        if (tengoInternet()) {
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            //String usr = String.valueOf(R.id.username);
+            //String pswd = String.valueOf(R.id.password);
+            // http://34.236.191.118:3000/api/v1/users/login?password="+pswd+"&email="+usr
+            String url = "http://34.236.191.118:3000/api/v1/users/login";
+
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            Log.d("infoWS", response);
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.d("error", error.getMessage());
+                        }
+                    }) {
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    HashMap<String, String> parametros = new HashMap<>();
+
+                    EditText editText = findViewById(R.id.username);
+                    String username = editText.getText().toString();
+
+                    editText = findViewById(R.id.password);
+                    String password = editText.getText().toString();
+
+                    parametros.put("password", password);
+                    parametros.put("email", username);
+                    return parametros;
+                }
+            };
+
+            requestQueue.add(stringRequest);
+        }
+    }
+
+    public void validarInternet(View view) {
+       Toast.makeText(this,tengoInternet()?"Si tengo internet":"No tengo internet", Toast.LENGTH_SHORT).show();
+    }
+
+    public boolean tengoInternet() {
+        ConnectivityManager connectivityManager =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        if (connectivityManager == null)
+            return false;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Network networks = connectivityManager.getActiveNetwork();
+            if (networks == null)
+                return false;
+
+            NetworkCapabilities networkCapabilities = connectivityManager.getNetworkCapabilities(networks);
+            if (networkCapabilities == null)
+                return false;
+
+            if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI))
+                return true;
+            if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR))
+                return true;
+            if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET))
+                return true;
+            return false;
+        } else {
+
+            NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+            if (activeNetworkInfo == null)
+                return false;
+            if (activeNetworkInfo.getType() == ConnectivityManager.TYPE_WIFI)
+                return true;
+            if (activeNetworkInfo.getType() == ConnectivityManager.TYPE_MOBILE)
+                return true;
+            if (activeNetworkInfo.getType() == ConnectivityManager.TYPE_ETHERNET)
+                return true;
+            return false;
+
+        }
     }
 }
