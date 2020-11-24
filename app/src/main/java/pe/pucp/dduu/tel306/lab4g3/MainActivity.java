4599c162317ef216df9bd3d0eaa6dda045f65a41
Alvaro Burga <a20150632@pucp.pe>
2020-11-23 23:00:40 -0500

    vcds

---
+++ app/src/main/java/pe/pucp/dduu/tel306/lab4g3/MainActivity.java
@@ -0,0 +1,15 @@
+package pe.pucp.dduu.tel306.lab4g3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //hola
+    }
+}
